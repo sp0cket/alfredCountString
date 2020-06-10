e412cd378c329dc4ed0a6c871e2f98b25ca26986
Aaron <qzh6662@gmail.com>
2020-06-10 17:54:46 +0800

    fix utf8 string count

--- count.py
+++ count.py
@@ -9,9 +9,9 @@ result = {
         "items": [
             {
                 "type": "file",
-                "title": "这个字符串包含%d个字符" % len(args),
+                "title": "这个字符串包含%d个字符" % len(args.decode('utf-8')),
                 "subtitle": "复制到剪贴板",
-                "arg": len(args),
+                "arg": len(args.decode('utf-8')),
             }
         ]}
 finalResult = json.dumps(result)
