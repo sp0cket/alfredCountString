89ca9c58192b0d9bbbfe535c23c98fa2cd05edbe
sp0cket <qzh6662@gmail.com>
2023-09-12 19:22:03 +0800

    Update count.py

--- count.py
+++ count.py
@@ -4,16 +4,15 @@ import sys
 import json
 
 args = sys.argv[1]
-
 result = {
         "items": [
             {
                 "type": "file",
-                "title": "这个字符串包含%d个字符" % len(args.decode('utf-8')),
+                "title": '这个字符串包含 {} 个字符'.format(len(args)),
                 "subtitle": "复制到剪贴板",
-                "arg": len(args.decode('utf-8')),
+                "arg": len(args),
             }
         ]}
 finalResult = json.dumps(result)
 
-print finalResult
+print(finalResult)
